136d00402d5c07c58760ee2808d082f5d7c508ca
itsmiraz <mirajulislamofficial044@gmail.com>
2024-03-06 20:28:52 +0600

    new

--- src/pages/_homepage/hero.jsx
+++ src/pages/_homepage/hero.jsx
@@ -1,13 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
 import { PrimaryButton } from '@/components/ui/Button'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Balancer from 'react-wrap-balancer'
+import { slideAnimation } from '@/lib/motion';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const Hero = () => {
+
+    const [sectionRef, inView] = useInView({
+        triggerOnce: true, // Trigger animation once when it enters the viewport
+        threshold: 0.2, // Adjust this threshold as needed
+    });
+
+    // State to control whether animations should play
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        if (inView) {
+            setAnimate(true);
+        }
+    }, [inView]);
+
     return (
-        <div className='flex justify-between px-4 md:px-20  grid-cols-2 gap-10 py-20'>
-            <div className='space-y-6 w-[50%]'>
+        <div ref={sectionRef} className='flex justify-between px-4 md:px-20  grid-cols-2 gap-10 py-20'>
+            <motion.div
+                initial='initial'
+                animate={animate ? 'animate' : 'initial'}
+                exit='exit'
+                variants={slideAnimation('left')}
+                className='space-y-6 w-[50%]'>
                 <p className='bg-third text-white px-4 py-1 rounded w-fit'>
                     Top Rated Local Roofer In Bolingbrook, Illinois.
                 </p>
@@ -22,14 +45,37 @@ const Hero = () => {
                 </p>
                 <PrimaryButton>GET A FREE QUOTE</PrimaryButton>
                 <div className='flex flex-wrap gap-x-10 mt-6'>
-                    <Image className=' drop-shadow-xl' src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709731261/resdium-assets/value%20home%20remodeling%20website%20assests/image_45_lgt6wl.webp'} alt='facebook-review' width={200} height={60} />
-                    <Image className='drop-shadow-xl' src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709731261/resdium-assets/value%20home%20remodeling%20website%20assests/image_46_yo1e6j.webp'} alt='facebook-review' width={200} height={60} />
+                    <motion.div
+                        initial='initial'
+                        animate={animate ? 'animate' : 'initial'}
+                        exit='exit'
+                        variants={slideAnimation('up')}>
+                        <Image className=' drop-shadow-xl' src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709731261/resdium-assets/value%20home%20remodeling%20website%20assests/image_45_lgt6wl.webp'} alt='facebook-review' width={200} height={60} />
+                    </motion.div>
+                    <motion.div
+                        initial='initial'
+                        animate={animate ? 'animate' : 'initial'}
+                        exit='exit'
+                        variants={slideAnimation('up')}>
+                        <Image className='drop-shadow-xl' src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709731261/resdium-assets/value%20home%20remodeling%20website%20assests/image_46_yo1e6j.webp'} alt='facebook-review' width={200} height={60} />
+                    </motion.div>
                 </div>
-            </div>
+            </motion.div>
             <div className='relative'>
-
-                <Image src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709729623/resdium-assets/value%20home%20remodeling%20website%20assests/hero-img-1_n1dyr6.webp'} width={507} height={428} alt='hero-main-img ' />
-                <Image className='absolute -bottom-[40%] -left-[45%]' src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709729603/resdium-assets/value%20home%20remodeling%20website%20assests/hero-img-2_hsrwzp.webp'} width={587} height={338} alt='hero-main-img ' />
+                <motion.div
+                    initial='initial'
+                    animate={animate ? 'animate' : 'initial'}
+                    exit='exit'
+                    variants={slideAnimation('right')}>
+                    <Image src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709729623/resdium-assets/value%20home%20remodeling%20website%20assests/hero-img-1_n1dyr6.webp'} width={507} height={428} alt='hero-main-img ' />
+                </motion.div>
+                <motion.div
+                    initial='initial'
+                    animate={animate ? 'animate' : 'initial'}
+                    exit='exit'
+                    variants={slideAnimation('left')}>
+                    <Image className='absolute w-full -bottom-[40%] -left-[45%]' src={'https://res.cloudinary.com/djnlyzsmv/image/upload/v1709729603/resdium-assets/value%20home%20remodeling%20website%20assests/hero-img-2_hsrwzp.webp'} width={587} height={338} alt='hero-main-img ' />
+                </motion.div>
             </div>
 
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Hero from "./_homepage/hero";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Value Home Remodeling</title>
+      </Head>
       <Hero />
     </main>
   );
